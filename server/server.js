c847b825d9361a7c774ec207277fcf3af4aa88c9
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-30 18:58:35 -0400

    Adds graphql schema

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const db = require("../config/keys.js").MONGO_URI;
-
+const models = require('./models');
 const app = express();
 
 if (!db) {
@@ -19,6 +19,21 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+
+    // Don't forget to import your schema from ./schema/schema
+    const expressGraphQL = require("express-graphql");
+    const schema = require("./schema/schema");
+
+    // ...
+    // ...
+
+    app.use(
+        "/graphql",
+        expressGraphQL({
+            schema,
+            graphiql: true
+        })
+    );
 // We use body-parser in order to be able to parse
 // incoming requests in middleware before they are handled
 app.use(bodyParser.json());
